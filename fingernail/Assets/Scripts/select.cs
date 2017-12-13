472febd6a67946f8f528d99968d112fc86f6f00a
pluto-wang <pluto@gmail.com>
2017-12-13 15:49:51 +0800

    An AR application developed by Unity and Vuforia

---
+++ fingernail/Assets/Scripts/select.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class select : MonoBehaviour {
+
+	public Dropdown myDropdown;
+	public GameObject normal;
+	public GameObject triangle;
+	public GameObject square;
+	public Material pink;
+	public Material red;
+	public Material black;
+	//public Renderer rend;
+
+	void Start() {		
+		
+		myDropdown.onValueChanged.AddListener (delegate {
+			myDropdownValueChangedHandler(myDropdown);
+		});
+	}
+
+	void Destroy() {
+		myDropdown.onValueChanged.RemoveAllListeners();
+	}
+
+	private void myDropdownValueChangedHandler(Dropdown target) {
+		
+		if (target.value == 0) {
+			Debug.Log ("selected color: pink");
+			normal.GetComponent<Renderer>().material=pink;
+			triangle.GetComponent<Renderer>().material=pink;
+			square.GetComponent<Renderer>().material=pink;
+
+		} else if (target.value == 1) {
+			Debug.Log ("selected color: red");
+			normal.GetComponent<Renderer>().material=red;
+			triangle.GetComponent<Renderer>().material=red;
+			square.GetComponent<Renderer>().material=red;
+
+
+		} else if (target.value == 2) {
+			Debug.Log ("selected color: black");
+			normal.GetComponent<Renderer>().material=black;
+			triangle.GetComponent<Renderer>().material=black;
+			square.GetComponent<Renderer>().material=black;
+
+		}
+	}
+
+	public void SetDropdownIndex(int index) {
+		myDropdown.value = index;
+	}
+}
+
---
+++ fingernail/Assets/Scripts/selectsh.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class selectsh : MonoBehaviour {
+
+	public Dropdown Dropdown;
+	public GameObject normal;
+	public GameObject triangle;
+	public GameObject square;
+
+	void Awake (){
+		//normal.TrackableEventHandler.isVisible == true;
+		normal.SetActive(true);
+		triangle.SetActive(false);
+		square.SetActive(false);
+	}
+
+	void Start() {		
+		Dropdown.onValueChanged.AddListener (delegate {
+			myDropdownValueChangedHandler(Dropdown);
+		});
+	}
+
+	void Destroy() {
+		Dropdown.onValueChanged.RemoveAllListeners();
+	}
+
+	private void myDropdownValueChangedHandler(Dropdown target) {
+		
+		if (target.value == 0) {
+			Debug.Log ("selected shape: normal");
+			normal.SetActive(true);
+			triangle.SetActive(false);
+			square.SetActive(false);
+
+		} else if (target.value == 1) {
+			Debug.Log ("selected shape: triangle");
+			normal.SetActive(false);
+			triangle.SetActive(true);
+			square.SetActive(false);
+
+		} else if (target.value == 2) {
+			Debug.Log ("selected shape: square");
+			normal.SetActive(false);
+			triangle.SetActive(false);
+			square.SetActive(true);
+
+		}
+	}
+
+	public void SetDropdownIndex(int index) {
+		Dropdown.value = index;
+	}
+}
+
